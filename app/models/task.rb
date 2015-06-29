5a3e16e158777f0c8a928d34271acfef23781817
Jennifer Bolanos & David Garber <student@epicodus.com>
2015-06-29 16:34:48 -0700

    Add 'mark done' feature to tasks.

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@ class Task < ActiveRecord::Base
   belongs_to :list
 
   validates :description, :presence => true
+  scope :done, -> {where(done: 'true')}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   resources :taks do
     resources :subtask
   end
+
+  match '/tasks/complete/:id' => 'tasks#complete', as: 'complete_task', via: :put
+  match '/tasks/complete/:id' => 'tasks#incomplete', as: 'incomplete_task', via: :delete
+
 end
