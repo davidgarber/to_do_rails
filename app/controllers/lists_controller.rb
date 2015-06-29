0a7d728a7359a2293b9c589e007e559916f43e10
Jennifer Bolanos & David Garber <student@epicodus.com>
2015-06-29 12:05:08 -0700

    Allow user to edit a list name.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,6 +4,11 @@ class ListsController < ApplicationController
     render :index
   end
 
+  def show
+    @list = List.find(params[:id])
+    render :show
+  end
+
 
   def new
     @list = List.new
@@ -22,6 +27,15 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
     render :edit
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to lists_path
+    else
+      render :edit
+    end
   end
 
     private
